b8b52b94ade6013f8fd541365ea33ecf0d014c2a
何磊 <405018981@qq.com>
2019-06-30 16:23:39 +0800

    add files

---
+++ DingTaisvr/src/main/java/com/halo/dingtaisvr/DingtaisvrApplication.java
@@ -0,0 +1,13 @@
+package com.halo.dingtaisvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingtaisvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingtaisvrApplication.class, args);
+    }
+
+}
